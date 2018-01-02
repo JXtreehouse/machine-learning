5d4980334a19bd3226b348ec2475bd894d2a6c46
szj46941 <szj46941@tc.com>
2018-01-02 16:32:04 +0800

    logistic regression

---
+++ action/logistic/logRegres.py
@@ -0,0 +1,50 @@
+from numpy import *
+
+def loadDataSet():
+    dataMat = []; labelMat = [];
+    fr = open('data/testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat,labelMat
+
+def sigmoid(input):
+    return 1.0/(1+exp(-input))
+
+def gradAscent(dataMatIn,classLabels):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    m,n = shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = ones((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = (labelMat - h)
+        weights = weights + alpha * dataMatrix.transpose() * error
+    return array(weights)
+
+def stocGradAscent0(dataMatrix,classLabels):
+    m, n = shape(dataMatrix)
+    alpha = 0.01
+    weights = ones(n)  # initialize to all ones
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i] * weights))
+        error = classLabels[i] - h
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+def stocGradAscent1(dataMatrix,classLabels,numIter=500):
+    m,n=shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            alpha = 4/(1.0+j+i)+0.0001
+            randIndex = int(random.uniform(0,len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error *dataMatrix[randIndex] #让randIndex位置的样本点J趋于局部最小
+            del (list(dataIndex)[randIndex])
+    return weights
---
+++ action/logistic/plot2D.py
@@ -0,0 +1,35 @@
+from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from action.logistic import logRegres
+
+dataMat,labelMat=logRegres.loadDataSet()
+dataArr = array(dataMat)
+weights = logRegres.stocGradAscent1(dataArr,labelMat)
+
+n = shape(dataArr)[0] #number of points to create
+xcord1 = []; ycord1 = []
+xcord2 = []; ycord2 = []
+
+markers =[]
+colors =[]
+for i in range(n):
+    if int(labelMat[i])== 1:
+        xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+    else:
+        xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+#ax.scatter(xcord,ycord, c=colors, s=markers)
+type1 = ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+type2 = ax.scatter(xcord2, ycord2, s=30, c='green')
+x = arange(-3.0, 3.0, 0.1) #生成从-3 到 3 的间隔0.1的数据
+y = (-weights[0]-weights[1]*x)/weights[2]
+type3 = ax.plot(x, y)
+#ax.legend([type1, type2, type3], ["Did Not Like", "Liked in Small Doses", "Liked in Large Doses"], loc=2)
+#ax.axis([-5000,100000,-2,25])
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.show()
