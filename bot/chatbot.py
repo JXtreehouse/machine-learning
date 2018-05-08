28da9190d18a2bd97e5b3f6c35c29718726891d1
szj46941 <szj46941@tc.com>
2018-05-08 16:48:42 +0800

    fetch

--- bot/chatbot.py
+++ bot/chatbot.py
@@ -114,8 +114,7 @@ def _construct_response(output_logits, metadata):
 
     This is a greedy decoder - outputs are just argmaxes of output_logits.
     """
-    print(output_logits[0])
-    outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+    outputs = [int(np.argmax(logit)) for logit in output_logits]
     # If there is an EOS symbol in outputs, cut them at that point.
     if config.EOS_ID in outputs:
         outputs = outputs[:outputs.index(config.EOS_ID)]
@@ -184,7 +183,7 @@ def _find_right_bucket(length):
 def chat():
     """ in test mode, we don't to create the backward path
     """
-    metadata, idx_q, idx_a = data.load_data()
+    test_data_buckets, train_data_buckets, train_buckets_scale, metadata = _get_buckets()
 
     model = ChatBotModel(True, batch_size=1)
     model.build_graph()
@@ -194,37 +193,22 @@ def chat():
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         _check_restore_parameters(sess, saver)
-        # Decode from standard input.
-        max_length = config.BUCKETS[-1][0]
-        print('Welcome to TensorBro. Say something. Enter to exit. Max length is', max_length)
-        while True:
-            try:
-                line = _get_user_input()
-                if len(line) > 0 and line[-1] == '\n':
-                    line = line[:-1]
-                if line == '':
-                    break
-                # Get token-ids for the input sentence.
-                token_ids = data.sentence2id(line, metadata['w2idx'])
-                if (len(token_ids) > max_length):
-                    print('Max length I can handle is:', max_length)
-                    continue
-                # Which bucket does it belong to?
-                bucket_id = _find_right_bucket(len(token_ids))
-                # Get a 1-element batch to feed the sentence to the model.
-                encoder_inputs, decoder_inputs, decoder_masks = data.get_batch([(token_ids, [])],
-                                                                               bucket_id,
-                                                                               batch_size=1)
-                # Get output logits for the sentence.
-                _, _, output_logits = run_step(sess, model, encoder_inputs, decoder_inputs,
-                                               decoder_masks, bucket_id, True)
-                response = _construct_response(output_logits, metadata)
-                print(response)
-            except BaseException:
-                print(BaseException)
+        bucket_id = _get_random_bucket(train_buckets_scale)
+        encoder_inputs, decoder_inputs, decoder_masks = data.get_batch(test_data_buckets[bucket_id],
+                                                                       bucket_id,
+                                                                       batch_size=20)
+
+        # Get output logits for the sentence.
+        _, _, output_logits = run_step(sess, model, encoder_inputs, decoder_inputs,
+                                       decoder_masks, bucket_id, True)
+
+        for logit in output_logits:
+
+            response = _construct_response(logit, metadata)
+            print(response)
 
 
-train_mode = True
+train_mode = False
 
 if __name__ == '__main__':
     if train_mode:
