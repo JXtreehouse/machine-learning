81276dacc8b27c9f77c1fe6016f1e509f5a68af4
szj <200765821@qq.com>
2018-06-10 21:25:43 +0800

    svm

---
+++ action/svm/noKernel.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+from numpy import *
+import matplotlib.pyplot as plt
+
+
+class optStruct:
+    def __init__(self, dataMatIn, classLabels, C, toler):  # Initialize the structure with the parameters
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m, 1)))
+        self.b = 0
+        self.eCache = mat(zeros((self.m, 2)))  # first column is valid flag
