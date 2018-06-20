2152295dbb8942286655a4a2a52655ee094d3677
szj46941 <szj46941@tc.com>
2018-06-20 16:25:32 +0800

    word2vec

--- 224n/a1/run.py
+++ 224n/a1/run.py
@@ -11,61 +11,64 @@ import time
 from word2vec import *
 from sgd import *
 
-# Reset the random seed to make sure that everyone gets the same results
-random.seed(314)
-dataset = StanfordSentiment()
-tokens = dataset.tokens()
-nWords = len(tokens)
 
-# We are going to train 10-dimensional vectors for this assignment
-dimVectors = 10
+if __name__ == '__main__':
+    # Reset the random seed to make sure that everyone gets the same results
+    random.seed(314)
+    dataset = StanfordSentiment()
+    tokens = dataset.tokens()
+    nWords = len(tokens)
 
-# Context size
-C = 5
+    # We are going to train 10-dimensional vectors for this assignment
+    dimVectors = 10
 
-# Reset the random seed to make sure that everyone gets the same results
-random.seed(31415)
-np.random.seed(9265)
+    # Context size
+    C = 5
 
-startTime=time.time()
-wordVectors = np.concatenate(
-    ((np.random.rand(nWords, dimVectors) - 0.5) /
-       dimVectors, np.zeros((nWords, dimVectors))),
-    axis=0)
-wordVectors = sgd(
-    lambda vec: word2vec_sgd_wrapper(skipgram, tokens, vec, dataset, C,
-        negSamplingCostAndGradient),
-    wordVectors, 0.3, 40000, None, True, PRINT_EVERY=10)
-# Note that normalization is not called here. This is not a bug,
-# normalizing during training loses the notion of length.
+    # Reset the random seed to make sure that everyone gets the same results
+    random.seed(31415)
+    np.random.seed(9265)
 
-print ("sanity check: cost at convergence should be around or below 10")
-print ("training took %d seconds" % (time.time() - startTime))
+    startTime = time.time()
+    wordVectors = np.concatenate(
+        ((np.random.rand(nWords, dimVectors) - 0.5) /
+         dimVectors, np.zeros((nWords, dimVectors))),
+        axis=0)
+    wordVectors = sgd(
+        lambda vec: word2vec_sgd_wrapper(skipgram, tokens, vec, dataset, C,
+                                         negSamplingCostAndGradient),
+        wordVectors, 0.3, 40000, None, True, PRINT_EVERY=10)
+    # Note that normalization is not called here. This is not a bug,
+    # normalizing during training loses the notion of length.
 
-# concatenate the input and output word vectors
-wordVectors = np.concatenate(
-    (wordVectors[:nWords,:], wordVectors[nWords:,:]),
-    axis=0)
-# wordVectors = wordVectors[:nWords,:] + wordVectors[nWords:,:]
+    print("sanity check: cost at convergence should be around or below 10")
+    print("training took %d seconds" % (time.time() - startTime))
 
-visualizeWords = [
-    "the", "a", "an", ",", ".", "?", "!", "``", "''", "--",
-    "good", "great", "cool", "brilliant", "wonderful", "well", "amazing",
-    "worth", "sweet", "enjoyable", "boring", "bad", "waste", "dumb",
-    "annoying"]
+    # concatenate the input and output word vectors
+    wordVectors = np.concatenate(
+        (wordVectors[:nWords, :], wordVectors[nWords:, :]),
+        axis=0)
+    # wordVectors = wordVectors[:nWords,:] + wordVectors[nWords:,:]
 
-visualizeIdx = [tokens[word] for word in visualizeWords]
-visualizeVecs = wordVectors[visualizeIdx, :]
-temp = (visualizeVecs - np.mean(visualizeVecs, axis=0))
-covariance = 1.0 / len(visualizeIdx) * temp.T.dot(temp)
-U,S,V = np.linalg.svd(covariance)
-coord = temp.dot(U[:,0:2])
+    visualizeWords = [
+        "the", "a", "an", ",", ".", "?", "!", "``", "''", "--",
+        "good", "great", "cool", "brilliant", "wonderful", "well", "amazing",
+        "worth", "sweet", "enjoyable", "boring", "bad", "waste", "dumb",
+        "annoying"]
 
-for i in range(len(visualizeWords)):
-    plt.text(coord[i,0], coord[i,1], visualizeWords[i],
-        bbox=dict(facecolor='green', alpha=0.1))
+    visualizeIdx = [tokens[word] for word in visualizeWords]
+    visualizeVecs = wordVectors[visualizeIdx, :]
+    temp = (visualizeVecs - np.mean(visualizeVecs, axis=0))
+    covariance = 1.0 / len(visualizeIdx) * temp.T.dot(temp)
+    U, S, V = np.linalg.svd(covariance)
+    coord = temp.dot(U[:, 0:2])
 
-plt.xlim((np.min(coord[:,0]), np.max(coord[:,0])))
-plt.ylim((np.min(coord[:,1]), np.max(coord[:,1])))
+    for i in range(len(visualizeWords)):
+        plt.text(coord[i, 0], coord[i, 1], visualizeWords[i],
+                 bbox=dict(facecolor='green', alpha=0.1))
 
-plt.savefig('q3_word_vectors.png')
+    plt.xlim((np.min(coord[:, 0]), np.max(coord[:, 0])))
+    plt.ylim((np.min(coord[:, 1]), np.max(coord[:, 1])))
+
+    plt.savefig('q3_word_vectors.png')
+
