2cdd88c90faafb47ac3ee7fd80c7694524b7ea35
孙证杰 <>
2019-10-06 14:25:50 +0800

    train

--- detection/train.py
+++ detection/train.py
@@ -1,8 +1,81 @@
+import os
 import tensorflow as tf
 import numpy as np
+import pickle
+
+train_stats = (
+    'Training statistics: \n'
+    '\tLearning rate : {}\n'
+    '\tBatch size    : {}\n'
+    '\tEpoch number  : {}\n'
+    '\tBackup every  : {}'
+)
+
+
+def _save_ckpt(self, step, loss_profile):
+    file = '{}-{}{}'
+    model = self.meta['name']
+
+    profile = file.format(model, step, '.profile')
+    profile = os.path.join(self.FLAGS.backup, profile)
+    with open(profile, 'wb') as profile_ckpt:
+        pickle.dump(loss_profile, profile_ckpt)
+
+    ckpt = file.format(model, step, '')
+    ckpt = os.path.join(self.FLAGS.backup, ckpt)
+    self.say('Checkpoint at step {}'.format(step))
+    self.saver.save(self.sess, ckpt)
+
+
+def train(self):
+    loss_ph = self.framework.placeholders
+    loss_mva = None
+    profile = list()
+
+    batches = self.framework.shuffle()
+    loss_op = self.framework.loss
+
+    for i, (x_batch, datum) in enumerate(batches):
+        if not i: self.say(train_stats.format(
+            self.FLAGS.lr, self.FLAGS.batch,
+            self.FLAGS.epoch, self.FLAGS.save
+        ))
+
+        feed_dict = {
+            loss_ph[key]: datum[key]
+            for key in loss_ph}
+        feed_dict[self.inp] = x_batch
+        feed_dict.update(self.feed)
+
+        fetches = [self.train_op, loss_op]
+
+        if self.FLAGS.summary:
+            fetches.append(self.summary_op)
+
+        fetched = self.sess.run(fetches, feed_dict)
+        loss = fetched[1]
+
+        if loss_mva is None: loss_mva = loss
+        loss_mva = .9 * loss_mva + .1 * loss
+        step_now = self.FLAGS.load + i + 1
+
+        if self.FLAGS.summary:
+            self.writer.add_summary(fetched[2], step_now)
+
+        form = 'step {} - loss {} - moving ave loss {}'
+        self.say(form.format(step_now, loss, loss_mva))
+        profile += [(loss, loss_mva)]
+
+        ckpt = (i + 1) % (self.FLAGS.save // self.FLAGS.batch)
+        args = [step_now, profile]
+        if not ckpt: _save_ckpt(self, *args)
+
+    if ckpt: _save_ckpt(self, *args)
+
 
 def expit_tensor(x):
-	return 1. / (1. + tf.exp(-x))
+    return 1. / (1. + tf.exp(-x))
+
 
 def loss(self, net_out):
     """
@@ -18,7 +91,7 @@ def loss(self, net_out):
     scoor = float(m['coord_scale'])
     H, W, _ = m['out_size']
     B, C = m['num'], m['classes']
-    HW = H * W # number of grid cells
+    HW = H * W  # number of grid cells
     anchors = m['anchors']
 
     print('{} loss hyper-parameters:'.format(m['model']))
@@ -43,38 +116,39 @@ def loss(self, net_out):
     _botright = tf.placeholder(tf.float32, size2 + [2])
 
     self.placeholders = {
-        'probs':_probs, 'confs':_confs, 'coord':_coord, 'proid':_proid,
-        'areas':_areas, 'upleft':_upleft, 'botright':_botright
+        'probs': _probs, 'confs': _confs, 'coord': _coord, 'proid': _proid,
+        'areas': _areas, 'upleft': _upleft, 'botright': _botright
     }
 
     # Extract the coordinate prediction from net.out
     net_out_reshape = tf.reshape(net_out, [-1, H, W, B, (4 + 1 + C)])
     coords = net_out_reshape[:, :, :, :, :4]
-    coords = tf.reshape(coords, [-1, H*W, B, 4])
-    adjusted_coords_xy = expit_tensor(coords[:,:,:,0:2])
-    adjusted_coords_wh = tf.sqrt(tf.exp(coords[:,:,:,2:4]) * np.reshape(anchors, [1, 1, B, 2]) / np.reshape([W, H], [1, 1, 1, 2]))
+    coords = tf.reshape(coords, [-1, H * W, B, 4])
+    adjusted_coords_xy = expit_tensor(coords[:, :, :, 0:2])
+    adjusted_coords_wh = tf.sqrt(
+        tf.exp(coords[:, :, :, 2:4]) * np.reshape(anchors, [1, 1, B, 2]) / np.reshape([W, H], [1, 1, 1, 2]))
     coords = tf.concat([adjusted_coords_xy, adjusted_coords_wh], 3)
 
     adjusted_c = expit_tensor(net_out_reshape[:, :, :, :, 4])
-    adjusted_c = tf.reshape(adjusted_c, [-1, H*W, B, 1])
+    adjusted_c = tf.reshape(adjusted_c, [-1, H * W, B, 1])
 
     adjusted_prob = tf.nn.softmax(net_out_reshape[:, :, :, :, 5:])
-    adjusted_prob = tf.reshape(adjusted_prob, [-1, H*W, B, C])
+    adjusted_prob = tf.reshape(adjusted_prob, [-1, H * W, B, C])
 
     adjusted_net_out = tf.concat([adjusted_coords_xy, adjusted_coords_wh, adjusted_c, adjusted_prob], 3)
 
-    wh = tf.pow(coords[:,:,:,2:4], 2) * np.reshape([W, H], [1, 1, 1, 2])
-    area_pred = wh[:,:,:,0] * wh[:,:,:,1]
-    centers = coords[:,:,:,0:2]
+    wh = tf.pow(coords[:, :, :, 2:4], 2) * np.reshape([W, H], [1, 1, 1, 2])
+    area_pred = wh[:, :, :, 0] * wh[:, :, :, 1]
+    centers = coords[:, :, :, 0:2]
     floor = centers - (wh * .5)
-    ceil  = centers + (wh * .5)
+    ceil = centers + (wh * .5)
 
     # calculate the intersection areas
-    intersect_upleft   = tf.maximum(floor, _upleft)
-    intersect_botright = tf.minimum(ceil , _botright)
+    intersect_upleft = tf.maximum(floor, _upleft)
+    intersect_botright = tf.minimum(ceil, _botright)
     intersect_wh = intersect_botright - intersect_upleft
     intersect_wh = tf.maximum(intersect_wh, 0.0)
-    intersect = tf.multiply(intersect_wh[:,:,:,0], intersect_wh[:,:,:,1])
+    intersect = tf.multiply(intersect_wh[:, :, :, 0], intersect_wh[:, :, :, 1])
 
     # calculate the best IOU, set 0.0 confidence for worse boxes
     iou = tf.truediv(intersect, _areas + area_pred - intersect)
@@ -90,13 +164,13 @@ def loss(self, net_out):
     proid = sprob * weight_pro
 
     self.fetch += [_probs, confs, conid, cooid, proid]
-    true = tf.concat([_coord, tf.expand_dims(confs, 3), _probs ], 3)
-    wght = tf.concat([cooid, tf.expand_dims(conid, 3), proid ], 3)
+    true = tf.concat([_coord, tf.expand_dims(confs, 3), _probs], 3)
+    wght = tf.concat([cooid, tf.expand_dims(conid, 3), proid], 3)
 
     print('Building {} loss'.format(m['model']))
     loss = tf.pow(adjusted_net_out - true, 2)
     loss = tf.multiply(loss, wght)
-    loss = tf.reshape(loss, [-1, H*W*B*(4 + 1 + C)])
+    loss = tf.reshape(loss, [-1, H * W * B * (4 + 1 + C)])
     loss = tf.reduce_sum(loss, 1)
     self.loss = .5 * tf.reduce_mean(loss)
     tf.summary.scalar('{} loss'.format(m['model']), self.loss)
